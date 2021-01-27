48603236387913c3760c7fd3f681856b9a6a30ed
Jordy <j.rosenbrand@nobears.nl>
2021-01-27 20:32:05 +0100

    reduce interface methods

--- src/Api/EndpointInterface.php
+++ src/Api/EndpointInterface.php
@@ -9,25 +9,9 @@ interface EndpointInterface
 {
     public function getUri(): string;
 
-    public function withUri(string $uri): EndpointInterface;
-
     public function getQueryParams(): array;
-
-    public function withQueryParams(array $queryParams = []): EndpointInterface;
 
     public function getPostBody();
 
-    public function withPostBody($body = null): EndpointInterface;
-
     public function getPrototype(): ResponseInterface;
-
-    public function getResponsePrototype(): ResponseInterface;
-
-    public function withResponsePrototype(ResponseInterface $response): EndpointInterface;
-
-    public function getResponseListPrototype(): ResponseListInterface;
-
-    public function withResponseListPrototype(ResponseListInterface $response): EndpointInterface;
-
-    public function transfer(string $httpMethod): ResponseInterface;
 }
