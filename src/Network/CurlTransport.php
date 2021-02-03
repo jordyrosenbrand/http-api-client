dd2ca7c173a89d849d89949ea062600a33214aec
Jordy <j.rosenbrand@nobears.nl>
2021-02-03 21:01:14 +0100

    refactor CurlTransport

--- src/Network/CurlTransport.php
+++ src/Network/CurlTransport.php
@@ -67,24 +67,6 @@ class CurlTransport implements TransportInterface
     }
 
     /**
-     * @param null $curlHandle
-     *
-     * @return $this
-     */
-    private function closeConnection($curlHandle = null)
-    {
-        $curlHandle = $curlHandle ?: $this->getCurlHandle(false);
-
-        if($curlHandle) {
-            curl_close($curlHandle);
-        }
-
-        unset($this->curlHandle);
-
-        return $this;
-    }
-
-    /**
      * @return bool
      */
     public function getAutoCloseConnection()
@@ -107,14 +89,45 @@ class CurlTransport implements TransportInterface
     /**
      * @param RequestInterface $request
      *
-     * @return TransportOutput
+     * @return TransportOutputInterface
      */
-    public function transfer(RequestInterface $request): TransportOutputInterface
-    {
-        $curlHandle = $this->getCurlHandle();
+    public function transfer(
+        RequestInterface $request
+    ): TransportOutputInterface {
         $headers = [];
 
-        curl_setopt_array($curlHandle, [
+        $data = $this->initCurl($request)
+            ->setCustomHttpMethod($request)
+            ->configureHeaderCallback(
+                function($curl, $header) use (&$headers) {
+                    $length = strlen($header);
+                    $header = explode(':', $header, 2);
+
+                    if(count($header) == 2) {
+                        $headers[strtolower(trim($header[0]))] = trim($header[1]);
+                    }
+
+                    return $length;
+                }
+            )
+            ->execute();
+
+        $output = $this->getOutput()
+            ->hydrate($headers, $data, $this->getInfo());
+
+        $this->close();
+
+        return $output;
+    }
+
+    /**
+     * @param RequestInterface $request
+     *
+     * @return $this
+     */
+    protected function initCurl(RequestInterface $request)
+    {
+        curl_setopt_array($this->getCurlHandle(), [
             CURLOPT_URL => $request->getQueriedUri(),
             CURLOPT_HTTPHEADER => $request->getHeaders(),
             CURLOPT_FOLLOWLOCATION => true,
@@ -122,26 +135,31 @@ class CurlTransport implements TransportInterface
             CURLOPT_HEADER => false,
             CURLOPT_POST => false,
             CURLOPT_CUSTOMREQUEST => null,
-            CURLOPT_HEADERFUNCTION => function($curl, $header) use (&$headers) {
-                $length = strlen($header);
-                $header = explode(':', $header, 2);
-
-                if(count($header) < 2) {
-                    return $length;
-                }
-
-                $headerName = strtolower(trim($header[0]));
-                $headerValue = trim($header[1]);
-
-                if(! isset($headers[$headerName])) {
-                    $headers[$headerName] = $headerValue;
-                } else {
-                    $headers[$headerName][] = $headerValue;
-                }
-
-                return $length;
-            }
         ]);
+
+        return $this;
+    }
+
+    /**
+     * @param callable $callback
+     *
+     * @return $this
+     */
+    protected function configureHeaderCallback(callable $callback)
+    {
+        curl_setopt($this->getCurlHandle(), CURLOPT_HEADERFUNCTION, $callback);
+
+        return $this;
+    }
+
+    /**
+     * @param RequestInterface $request
+     *
+     * @return $this
+     */
+    protected function setCustomHttpMethod(RequestInterface $request)
+    {
+        $curlHandle = $this->getCurlHandle();
 
         if($request->isPost()) {
             curl_setopt_array($curlHandle, [
@@ -163,13 +181,52 @@ class CurlTransport implements TransportInterface
             ]);
         }
 
-        $data = curl_exec($curlHandle);
-        $info = curl_getinfo($curlHandle);
+        return $this;
+    }
 
+    /**
+     * @return bool|string
+     */
+    protected function execute()
+    {
+        return curl_exec($this->getCurlHandle());
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getInfo()
+    {
+        return curl_getinfo($this->getCurlHandle());
+    }
+
+    /**
+     * @return $this
+     */
+    protected function close()
+    {
         if($this->getAutoCloseConnection()) {
-            $this->closeConnection($curlHandle);
+            $this->closeConnection();
         }
 
-        return $this->getOutput()->hydrate($headers, $data, $info);
+        return $this;
+    }
+
+    /**
+     * @param null $curlHandle
+     *
+     * @return $this
+     */
+    private function closeConnection($curlHandle = null)
+    {
+        $curlHandle = $curlHandle ?: $this->getCurlHandle(false);
+
+        if($curlHandle) {
+            curl_close($curlHandle);
+        }
+
+        unset($this->curlHandle);
+
+        return $this;
     }
 }
